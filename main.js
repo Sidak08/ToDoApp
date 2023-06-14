f7692a037ebfae37a75bebfb788a95b736094c06
Sidak08 <magicalsidak@gmail.com>
2023-06-13 20:28:47 -0400

    got the retrive data to work

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 let numberDiv = 1;
+let idea = [];
+let savedIdeaArray = [];
 
 function generateDiv() {
   console.log(numberDiv);
 
-  const textHolder = document.createElement("input");
+  let textHolder = document.createElement("input");
   textHolder.setAttribute('placeholder', `This is your ${numberDiv} idea`);
   textHolder.setAttribute("id", `idea${numberDiv}`)
 
   document.body.appendChild(textHolder);
-
-  numberNumDiv = numberDiv + 1;
-  //saveUserInput()
-
+  numberDiv = numberDiv + 1;
 }
 
-// function saveUserInput() {
+function pushIdea() {
+  idea = [];
+  for (let i = 1; i < numberDiv; i++) {
+    let inputValue = document.getElementById(`idea${i}`).value;
+    idea.push(inputValue);
+  }
+  console.log(idea);
+  saveUserInput();
+}
 
-//   const userInput = document.getElementById('userInput').value;
-//   localStorage.setItem('savedInput', userInput);
-//   console.log('User input saved to local storage.');
-// }
+function saveUserInput() {
+  localStorage.setItem('savedInput', JSON.stringify(idea));
+  console.log('User input saved to local storage.');
+}
+
+function retrieveUserInput() {
+  const savedInput = localStorage.getItem('savedInput');
+  savedIdeaArray = JSON.parse(savedInput);
+  console.log('Retrieved user input:', savedIdeaArray);
+  displayInfo();
+}
+
+function displayInfo() {
+  numberDiv = savedIdeaArray.length + 1;
+  retrivediv();
+  
+}
+
+function retrivediv() {
+  for (let i = 0; i < 3; i++) {
+    let textHolder = document.createElement("input");
+    textHolder.setAttribute('placeholder', `This is your ${i + 1} idea`);
+    textHolder.setAttribute("id", `idea${i + 1}`);
+    textHolder.value = savedIdeaArray[i]
+   
+
+    document.body.appendChild(textHolder);
+  }
+}
